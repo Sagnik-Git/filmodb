bded6e813ccb043dfa81e5667e58a0026f0ca1e5
Sagnik-Git <93276127+Sagnik-Git@users.noreply.github.com>
2023-10-14 06:38:30 +0530

    Update Trending.jsx

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -4,7 +4,7 @@ import Carousel from "../../../components/carousel/Carousel";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 
-import useFetch from "../../../hooks/useFetch";
+import useFetch from "../../../hooks/UseFetch";
 
 const Trending = () => {
     const [endpoint, setEndpoint] = useState("day");
