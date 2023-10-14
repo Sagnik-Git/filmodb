16a1c7e9ba5d9b81cf75d487fd9ce3c018f50d06
Sagnik-Git <93276127+Sagnik-Git@users.noreply.github.com>
2023-10-14 06:44:53 +0530

    Update Popular.jsx

--- src/pages/home/popular/Popular.jsx
+++ src/pages/home/popular/Popular.jsx
@@ -4,7 +4,7 @@ import Carousel from "../../../components/carousel/Carousel";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 
-import useFetch from "../../../hooks/useFetch";
+import useFetch from "../../../hooks/UseFetch";
 
 const Popular = () => {
     const [endpoint, setEndpoint] = useState("movie");
