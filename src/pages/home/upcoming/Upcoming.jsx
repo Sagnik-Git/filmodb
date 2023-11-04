2c6f5e8ca0385a553db8556c96163428f0e3c533
Sagnik-Git <93276127+Sagnik-Git@users.noreply.github.com>
2023-11-04 19:09:19 +0530

    Update Upcoming.jsx

--- src/pages/home/upcoming/Upcoming.jsx
+++ src/pages/home/upcoming/Upcoming.jsx
@@ -21,25 +21,25 @@ dateNxtMnth.setDate(currentDate.getDate() + 30);
 const dateNxtWkString = dateNxtWk.toISOString().split('T')[0]; // Example: "2023-11-11"
 const dateNxtMnthString = dateNxtMnth.toISOString().split('T')[0]; // Example: "2023-12-04"
 
-//console.log("Date after 7 days:", dateAfter7DaysString);
-//console.log("Date after 30 days:", dateAfter30DaysString);
+//console.log("Date after 7 days:" dateNxtWkString);
+//console.log("Date after 30 days:", dateNxtMnthString);
 
 
 const Upcoming = () => {
     const [endpoint, setEndpoint] = useState("en");
 
     const { data, loading } = 
-    useFetch(`/movie/upcoming?primary_release_date.gte=currentDate&primary_release_date.lte=${}`);
+    useFetch(`/movie/upcoming?primary_release_date.gte=${currentDate}&primary_release_date.lte=${endpoint}`);
 
     const onTabChange = (tab) => {
-        setEndpoint(tab === "English" ? "en" : "hi");
+        setEndpoint(tab === Week ? dateNxtWkString : dateNxtMnthString);
     };
 
     return (
         <div className="carouselSection">
             <ContentWrapper>
                 <span className="carouselTitle">Upcoming Titles</span>
-                <SwitchTabs data={["English", "Hindi"]} onTabChange={onTabChange} />
+                <SwitchTabs data={["Week", "Month"]} onTabChange={onTabChange} />
             </ContentWrapper>
             <Carousel data={data?.results} 
             loading={loading} 
